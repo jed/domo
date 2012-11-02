cd354766f4e9689b002f0e4f44e85c58876ed746
Jed Schmidt <tr@nslator.jp>
2012-11-02 21:06:58 +0900

    inline domo

--- build.js
+++ build.js
@@ -5,38 +5,33 @@ var uglify = require("uglify-js")
 var coffee = require("coffee-script")
 var domo   = require("./")
 
-build()
+compileJS()
+compileHTML()
 
-function build() {
-  compileJS(throwError)
-  compileHTML(throwError)
-}
-
-function compileJS(cb) {
+function compileJS() {
   var csPath = path.resolve(__dirname, "docs", "index.coffee")
   var jsPath = path.resolve(__dirname, "docs", "index.js")
+  var cs = fs.readFileSync(csPath, "utf8")
+  var js = coffee.compile(cs, {bare: true})
 
-  fs.readFile(csPath, "utf8", function(err, data) {
-    if (err) return cb(err)
-
-    try { var js = coffee.compile(data, {bare: true}) }
-    catch (err) { return cb(err) }
-
-    fs.writeFile(jsPath, uglify(js), cb)
-  })
+  fs.writeFileSync(jsPath, uglify(js))
 }
 
-function compileHTML(cb) {
+function compileHTML() {
   var htmlPath = path.resolve(__dirname, "index.html")
+  var domoPath = path.resolve(__dirname, "lib", "domo.js")
 
-  getSize(function(err, size) {
-    if (err) return cb(err)
+  var domo = fs.readFileSync(domoPath, "utf8")
+  var minifiedDomo = uglify(domo)
+
+  zlib.gzip(minifiedDomo, function(err, data) {
+    if (err) throw err
 
     var dom =
 
     DOCUMENT(
-      SCRIPT({src: "lib/domo.js"}),
-      SCRIPT("domo.size", "=", size),
+      SCRIPT(minifiedDomo),
+      SCRIPT("domo.size", "=", minifiedDomo.length),
       SCRIPT({src: "docs/index.js", charset: "utf-8"}),
 
       NOSCRIPT(
@@ -47,24 +42,6 @@ function compileHTML(cb) {
       )
     )
 
-    fs.writeFile(htmlPath, dom.outerHTML, cb)
+    fs.writeFileSync(htmlPath, dom.outerHTML)
   })
-}
-
-function getSize(cb) {
-  var domoPath = path.resolve(__dirname, "lib", "domo.js")
-
-  fs.readFile(domoPath, "utf8", function(err, data) {
-    if (err) return cb(err)
-
-    zlib.gzip(uglify(data), function(err, data) {
-      if (err) return cb(err)
-
-      cb(null, data.length)
-    })
-  })
-}
-
-function throwError(err) {
-  if (err) throw err
 }
